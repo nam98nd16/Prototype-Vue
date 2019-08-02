eb50bf61d57763b004c85eec3ab34773f6257fda
nam98nd16 <nam98nd16@gmail.com>
2019-08-02 11:47:47 +0700

    add Navigation bar

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue';
-
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+Vue.component('v-icon', Icon)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
@@ -14,6 +16,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: { App, 'v-icon': Icon },
   template: '<App/>'
 })
